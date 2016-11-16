dd6bb05a03c7cd0a7b33ebc68df71d0d2005b447
Joe <joseph.fehrman@gmail.com>
2016-11-15 20:30:52 -0500

    Created a checkfield for dynamic list.
     Changes to be committed:
    	new file:   src/form_elements/CheckField.js
    	new file:   src/test/form_elements/CheckField/Construct_Test.js
    	new file:   src/test/form_elements/CheckField/index.html

---
+++ src/form_elements/CheckField.js
@@ -0,0 +1,62 @@
+/**
+  Create a check input.
+
+  @author Joe Fehrman
+  @since 11/14/2016
+*/
+var CheckField = function(inputDescription){
+
+  /** Default constructor. */
+  function __construct(inputDescription){
+    var wrapper = createWrapperSpan();
+    var label = createLabel(inputDescription.id, inputDescription.label);
+    var element = createElements(inputDescription.id, inputDescription.values);
+    wrapper.appendChild(label);
+    wrapper.appendChild(element);
+    return wrapper;
+  }
+
+  /** Create a wrapper element. */
+  function createWrapperSpan(){
+    var wrapper = document.createElement("span");
+    wrapper.classList.add("dynamic_check");
+    return wrapper;
+  }
+
+  /**
+    Create a label.
+
+    @param id Id of the input.
+    @param labelText Label test show in display.
+    @return Label element.
+  */
+  function createLabel(id, labelText){
+    var label = document.createElement("label");
+    label.setAttribute("for", id);
+    var textNode = document.createTextNode(labelText);
+    label.appendChild(textNode);
+    return label;
+  }
+
+  /**
+    Create the input element.
+
+  */
+  function createElements(id, listOfValues){
+    var wrapper = document.createElement("span");
+    for(var i = 0; i < listOfValues.length; i++){
+      var item = listOfValues[i];
+      var input = document.createElement("input");
+      input.setAttribute("type", "checkbox");
+      input.setAttribute("name", id);
+      input.setAttribute("value", item.value);
+      input.classList.add("c-field");
+      var textNode = document.createTextNode(item.label);
+      input.appendChild(textNode);
+      wrapper.appendChild(input);
+    }
+    return wrapper;
+  }
+
+  return __construct(inputDescription); // Call the constructor.
+}
